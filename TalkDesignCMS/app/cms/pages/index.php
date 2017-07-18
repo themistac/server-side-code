6556c4059590884e2886e7076d6bdad1336efe85
Marcus Corbridge <marcuscorbridge@gmail.com>
2017-07-18 10:35:46 +0100

    Update index.php

--- TalkDesignCMS/app/cms/pages/index.php
+++ TalkDesignCMS/app/cms/pages/index.php
@@ -53,8 +53,8 @@ include '../common/breadcrumb.php';
 ?>
 
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="eng">
+<!DOCTYPE html>
+<html>
 <head>
 <title><?php echo $page_we_are_on ?></title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
