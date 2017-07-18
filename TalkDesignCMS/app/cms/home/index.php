739f7898d792ac5b986d4afa56a485e998fdc055
Marcus Corbridge <marcuscorbridge@gmail.com>
2017-07-18 10:34:30 +0100

    Update index.php

--- TalkDesignCMS/app/cms/home/index.php
+++ TalkDesignCMS/app/cms/home/index.php
@@ -4,11 +4,11 @@ include '../common/config.php';
 include '../common/opendb.php';
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="eng">
+<!DOCTYPE html>
+<html>
 <head>
-<title>konduceCMS by kdesigngroup</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>konduceCMS by Talk Design & Print</title>
+<meta charset="UTF-8" />
 
 <link rel="stylesheet" type="text/css" media="screen" href="../common/screen.css" />
 
