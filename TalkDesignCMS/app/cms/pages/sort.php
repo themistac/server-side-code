0252bb216cd5a1a4593d8994786c4d6f7a7e3027
Marcus Corbridge <marcuscorbridge@gmail.com>
2017-07-18 10:36:20 +0100

    Update sort.php

--- TalkDesignCMS/app/cms/pages/sort.php
+++ TalkDesignCMS/app/cms/pages/sort.php
@@ -1,6 +1,6 @@
 <?php
-$connection = mysql_connect ("localhost", "talkde39_talkdbu", "Lwa0XmX+yXq;");
-$db= mysql_select_db("talkde39_talk",$connection) or die("no db found");
+$connection = mysql_connect ("localhost", "******", "******");
+$db= mysql_select_db("******",$connection) or die("no db found");
 foreach ($_POST[sortlist] as $varname => $varvalue) {
 	$sql = "update kkwinch_content set ordering = ".mysql_real_escape_string($varname)." where id = ".mysql_real_escape_string($varvalue);
 	$result = mysql_query($sql) or die(mysql_error());
